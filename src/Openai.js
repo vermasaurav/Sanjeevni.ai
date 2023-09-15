ec997d406630aaa12a972271e3de4ec91e575efe
saurav <vermasourav120@gmail.com>
2023-09-15 21:55:03 +0530

    third commit

--- src/Openai.js
+++ src/Openai.js
@@ -1,13 +1,16 @@
-// import {Configuration, OpenAIApi} from 'openai';
-const {Configuration, OpenAIApi}=require('openai');
+import {Configuration, OpenAIApi} from 'openai';
+// const {Configuration, OpenAIApi}=require('openai');
 const configuration = new Configuration({ apiKey: "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"});
 const openai = new OpenAIApi(configuration);
+
+
+
 
 
 export async function sendMsg(message) {
     const res = await openai.createCompletion({
         model: 'text-davinci-003',
-        prompt: message,
+        prompt: "hello",
         temperature: 0.7,
         max_token: 256,
         top_p: 1,
--- src/components/homepage/Input.jsx
+++ src/components/homepage/Input.jsx
@@ -1,28 +1,31 @@
-import {React,useState} from 'react'
-import { sendMsg } from '../../Openai';
+import { React, useState, useReducer } from 'react'
+import { sendMsg } from '../../openai';
 import send from '../../assests/Group 3 new.svg';
 import syncBtn from '../../assests/Group 6 new.svg';
 import '../../App.css';
 
 
 const Input = () => {
-  const [input,setInput] = useState("");
+  const [input, setInput] = useState("");
 
   const handledSend = async () => {
     const res = await sendMsg(input);
     console.log(res);
   }
-  
+
+
   return (
     <>
-    <div>
-      <input type='text' placeholder='Write your query here....' className='inp' value={input} onChange={(e)=>{setInput(e.target.value)}}/>
-    </div>
-    <div className='bTn'>
-      <button   className='sendBtn' onClick={handledSend}><img src={send} alt='sendImage' className='Sendimg' /></button>
-      <button className='syncBtn'><img src={syncBtn} alt='sync' className='syncimg'/></button>
+      <div className='inp-main'>
+        
+          <input type='text' placeholder='Write your query here....' className='inp' value={input} onChange={(e) => { setInput(e.target.value) }} />
+       
+        
+          <button className='sendBtn' onClick={handledSend}><img src={send} alt='sendImage' className='Sendimg' /></button>
+          <button className='syncBtn'><img src={syncBtn} alt='sync' className='syncimg' /></button>
+        
       </div>
-      </>
+    </>
   )
 }
 
